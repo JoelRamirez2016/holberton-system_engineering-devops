513ffd120d1d4f968b86be6710eb82e006a51270
JoelRamirez2016 <joel.ramirez@correounivalle.edu.co>
2021-09-29 20:23:49 +0000

    add 0 0x17

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,5 @@
+# Using Puppet, create a manifest that change phpp -> php in wp-settings
+exec {
+    "sed -i 's/phpp/php/' ../var/www/html/wp-settings.php":
+    path => '/usr/bin/:/usr/local/bin/:/bin/'
+}
